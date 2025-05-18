b0b6ccd6e330da8e5eebddd456051c4271f05154
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2025-05-18 16:19:41 +0500

    Add files via upload

---
+++ loops practice .py
@@ -0,0 +1,8 @@
+#for i in ['abdullah','khan','azhar']:
+#    print(i)
+x=1
+while x<=3:
+    print(x)
+    x+=1
+    for first_name  in ['abdullah','ahmad','azhar']:
+        for last_name in ['khan','javad','khalil']:
